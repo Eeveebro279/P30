67ed694a17f77723b00a847ea294f2938b1fd35f
Eeveebro279 <71796267+Eeveebro279@users.noreply.github.com>
2021-02-10 18:59:38 -0500

    Add files via upload

---
+++ Js/Block.js
@@ -0,0 +1,30 @@
+class Block {
+  constructor(x,y,width,height) {
+    var options = {
+        restitution: 0.4
+    }
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+    this.visibility = 225
+  }
+  display(){
+    if(this.body.speed < 3){
+    var pos =this.body.position;
+    var  angle = this.body. angle;
+    push ();
+    translate (pos.x, pos.y)
+    rotate (angle)
+    rectMode(CENTER);
+    fill("brown");
+    rect(0, 0, this.width, this.height);
+    pop (); }
+    else{
+      World.remove(world, this.body)
+      push();
+      this.visibility = this.visibility - 10
+      pop ();
+    }
+  }
+};
